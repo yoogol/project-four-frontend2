1e0586623f22c2aacc02252f19d3af3c91172b21
Yulia Shea <yulia.shea@gmail.com>
2016-05-05 21:48:24 -0400

    pushing to heroku

--- app/components/AboutApp.js
+++ app/components/AboutApp.js
@@ -8,7 +8,10 @@ require('../style/Styles.css');
 const AboutApp = React.createClass ({
   render: function() {
     return (
-      <div>About App</div>
+      <div className="aboutText"><p>Purple Boom is one girl's dream come true.</p><p>Since the age of 15 I dreamt about a tool that would help me easily see and manage all of my clothes that were sadly trapped in my tiny, crowded closet. More than that, I dreamt about building such a tool myself.</p>
+      <p>This app was designed as part of my final project at General Assembly immersive fullstack development program where I learned web programming from scratch in 3 months.</p>
+      <p><a className="linkLightBG" href="https://www.linkedin.com/in/yuliashea">Yulia Shea</a></p>
+      </div>
       )}
   })
 
